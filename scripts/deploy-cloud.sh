82c0c9c7fdf4fb0731416b9b097181d1b6bda4a8
Tu1231 <1799405509@qq.com>
2025-11-26 19:31:21 +0800

    update:cloud deploy, and add dist gitignore

--- scripts/deploy-cloud.sh
+++ scripts/deploy-cloud.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-
+# ================== 服务器 ===================
+# 项目目录: /home/Turw-docs/
+# 服务器定期执行（用户级cron-每天凌晨执行一次）
+# crontab -e
+# 0 0 * * * /home/Turw-docs/deploy-cloud.sh > /dev/null 2>&1
+# ============================================
 cd /home/Turw-docs/
 
+
 git pull --rebase
+
+# 检查代码更新...
+if [ $? -eq 0 ] && git status | grep -q "up to date"; then
+   # 没有更新
+   exit 0
+fi
 
 pnpm i
 
 pnpm run build
 
 /www/server/nginx/sbin/nginx -s reload
+
